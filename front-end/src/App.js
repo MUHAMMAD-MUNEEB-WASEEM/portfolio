284b3d005bef4c9a0632c74f620ee7bf7d8dd472
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-03-27 11:02:24 +0500

    frontend basics done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,23 @@
 
-import './App.css';
+import './App.scss';
+import Navbar from './components/Navbar/Navbar';
+import About from './container/About/About';
+import Footer from './container/Footer/Footer';
+import Header from './container/Header/Header';
+import Skills from './container/Skills/Skills';
+import Testimonials from './container/Testimonials/Testimonials';
+import Work from './container/Work/Work';
 
 function App() {
   return (
-    <div>
-      App
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonials/>
+      <Footer/>
     </div>
   );
 }
