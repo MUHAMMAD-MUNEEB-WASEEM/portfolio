11b6695cda920fde5743d1b33231fa51e5ca7c6d
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-03-28 12:21:15 +0500

    done with about section

--- front-end/src/container/About/About.jsx
+++ front-end/src/container/About/About.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './About.scss'
 import { motion } from 'framer-motion'
 import {images} from '../../constants/index'
 
+import {urlFor, client} from '../../sanity';
+
 function About() {
   
-  const abouts = [
-    { title: 'web Development', description: 'I am a good web developer', imgUrl: images.about01},
-    { title: 'web Desing', description: 'I am a good web developer', imgUrl: images.about02},
-    { title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03},
-    { title: 'Web Animations', description: 'I am a good web developer', imgUrl: images.about04},
-  ] 
+  // const abouts = [
+  //   { title: 'web Development', description: 'I am a good web developer', imgUrl: images.about01},
+  //   { title: 'web Desing', description: 'I am a good web developer', imgUrl: images.about02},
+  //   { title: 'UI/UX', description: 'I am a good web developer', imgUrl: images.about03},
+  //   { title: 'Web Animations', description: 'I am a good web developer', imgUrl: images.about04},
+  // ] 
+
+  const [abouts, setAbouts] = useState([]);
+
+
+  useEffect(()=> {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+    .then((data)=>{
+      setAbouts(data);
+    })
+  },[])
 
 
   return (
@@ -28,7 +42,7 @@ function About() {
             key={about.title + index}
             >
 
-              <img src={about.imgUrl} alt={about.title} />
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
               <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
           </motion.div>
